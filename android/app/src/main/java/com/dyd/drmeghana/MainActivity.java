9e49a5473e2e56ab6e902f56fc94650a18e4afb0
GauravShastrakar33 <155727305+GauravShastrakar33@users.noreply.github.com>
2026-02-03 12:46:09 +0000

    badges updated

--- android/app/src/main/java/com/dyd/drmeghana/MainActivity.java
+++ android/app/src/main/java/com/dyd/drmeghana/MainActivity.java
@@ -1,5 +1,25 @@
 package com.dyd.drmeghana;
-
 import com.getcapacitor.BridgeActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.net.http.SslError;
+import android.webkit.SslErrorHandler;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        getBridge().getWebView().setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                // Load local offline page instead of showing URL error
+                view.loadUrl("file:///android_asset/offline.html");
+            }
+        });
+    }
+}
+
+
